461b30655754ec0349e0dae6218c04aa48ffaff3
nicolinedau <54591425+nicolinedau@users.noreply.github.com>
2019-12-09 10:21:22 +0100

    Login

--- js/main.js
+++ js/main.js
@@ -5,3 +5,34 @@ function validateForm() {
     return false;
   }
 }
+
+window.onload = function () {
+
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	theme: "light2", // "light1", "light2", "dark1", "dark2"
+	title:{
+	},
+	axisY: {
+		title: "Reserves(MMbbl)"
+	},
+	data: [{        
+		type: "column",  
+		showInLegend: true, 
+		legendMarkerColor: "blue",
+		legendText: "MMbbl = one million barrels",
+		dataPoints: [      
+			{ y: 600, label: "Venezuela" },
+			{ y: 266455,  label: "Saudi" },
+			{ y: 169709,  label: "Canada" },
+			{ y: 158400,  label: "Iran" },
+			{ y: 142503,  label: "Iraq" },
+			{ y: 101500, label: "Kuwait" },
+			{ y: 97800,  label: "UAE" },
+			{ y: 80000,  label: "Russia" }
+		]
+	}]
+});
+chart.render();
+
+}
