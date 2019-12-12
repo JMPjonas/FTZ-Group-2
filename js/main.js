52dae28487d8fd72e1811cf78f5029eea47a79f2
Joachim <joachimpoulsen33@gmail.com>
2019-12-12 12:38:45 +0100

    Rettet javascript
    Mit script kørte før DOM var blevet loaded så det virkede selvfølgelig ikke

--- js/main.js
+++ js/main.js
@@ -1,24 +1,5 @@
-function validateForm() {
-  var x = document.forms["myForm"]["fname"].value;
-  if (x == "") {
-    alert("Name must be filled out");
-    return false;
-  }
-}
-/* Ida Start */ 
 
-/*Ida Slut*/
-
-/*Nicoline start*/
-const nummerplade = document.getElementById('nummerplade')
-const password = document.getElementById('adgangskode')
-
-
-
-
-/*Nicoline  slut*/
-
-/* JONAS START*/
+/* JONAS SLUT*/
 
 /* JONAS SLUT*/
 /* JOACHIM START*/
@@ -36,6 +17,7 @@ function change(){
     
 }
 
-
+var drinks = ["ginhass", "mojito", "Darknstormy"];
+document.getElementById("cocktails").innerHTML=drinks[0]
 
 /* JOACHIM SLUT*/
